8f4360635dec34126d1980fe44beba2c0213588b
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-07 13:06:29 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+
+const display = document.getElementById('display');
+
+
+function appendToDisplay(value) {
+	display.value += value;
+}
+
+function clearDisplay() {
+	display.value = '';
+}
+
+function deleteLast() {
+	display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+	try {
+		display.value = eval(display.value);
+	} catch (error) {
+		display.value = 'Error';
+	}
+}
+
